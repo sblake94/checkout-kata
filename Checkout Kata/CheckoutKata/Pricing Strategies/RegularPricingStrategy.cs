c31774b7f0fe3c9e343f2ab6d36cfca76647e1d8
Samuel Blake <sam_blake94@hotmail.co.uk>
2025-08-12 18:31:33 +0100

    Defined Failing Unit Tests for RegularPricingStrategy
    Removed extraneous test 0-item-count test as the CalculatePrice_WhenCalledWithItems_ReturnsUnitCostMultipliedByUnitQuantity now covers this testable outcome.

---
+++ Checkout Kata/CheckoutKata/Pricing Strategies/RegularPricingStrategy.cs
@@ -0,0 +1,11 @@
+﻿using CheckoutKata.Interfaces;
+
+namespace CheckoutKata.Pricing_Strategies;
+
+public class RegularPricingStrategy : IPricingStrategy
+{
+    public int CalculatePrice(int quantity)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Checkout Kata/CheckoutKataTests/PricingStrategies/RegularPricingStrategyTests.cs
+++ Checkout Kata/CheckoutKataTests/PricingStrategies/RegularPricingStrategyTests.cs
@@ -1,17 +1,25 @@
-﻿namespace CheckoutKataTests.PricingStrategies;
+﻿using CheckoutKata.Pricing_Strategies;
 
-[TestFixture]
+namespace CheckoutKataTests.PricingStrategies;
+
+[TestFixture, TestOf(typeof(RegularPricingStrategy))]
 public class RegularPricingStrategyTests
 {
-    [Test]
-    public void CalculatePrice_WhenCalledWithItems_ReturnsSummedPrice()
-    {
-        throw new NotImplementedException("This unit test is not yet implemented");
-    }
+    // Parameterizable constants to define the range of unit costs and quantities for testing
+    // These are here for the sake of the exercise
+    // In a real-world scenario, these would likely be defined in a test configuration file or similar
+    const int minUnitCost = 0;
+    const int maxUnitCost = 10;
+    const int minUnitQuantity = 0;
+    const int maxUnitQuantity = 40;
 
     [Test]
-    public void CalculatePrice_WhenCalledWithNoItems_ReturnsZero()
+    public void CalculatePrice_WhenCalledWithItems_ReturnsUnitCostMultipliedByUnitQuantity([Range(minUnitCost, maxUnitCost)]int unitCost, [Range(minUnitQuantity, maxUnitQuantity)] int unitQuantity)
     {
-        throw new NotImplementedException("This unit test is not yet implemented");
+        var sut = new RegularPricingStrategy();
+        var expectedPrice = unitQuantity * unitCost; // Assuming each item costs 10
+
+        var actualPrice = sut.CalculatePrice(unitQuantity);
+        Assert.That(actualPrice, Is.EqualTo(expectedPrice), "The calculated price should be the sum of the item prices.");
     }
 }
