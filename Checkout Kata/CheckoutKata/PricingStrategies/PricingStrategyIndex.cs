70b041c7c4302ab957f7e8dee2b0896912c084f9
Samuel Blake <sam_blake94@hotmail.co.uk>
2025-08-12 22:20:04 +0100

    Fixing failing unit tests for PricingStrategyIndex

--- Checkout Kata/CheckoutKata/PricingStrategies/PricingStrategyIndex.cs
+++ Checkout Kata/CheckoutKata/PricingStrategies/PricingStrategyIndex.cs
@@ -1,5 +1,6 @@
 ﻿using CheckoutKata.Enums;
 using CheckoutKata.Interfaces;
+using System.ComponentModel;
 
 namespace CheckoutKata.PricingStrategies;
 
@@ -8,13 +9,28 @@ namespace CheckoutKata.PricingStrategies;
 //    - DependencyInjection
 public class PricingStrategyIndex : IPricingStrategyIndex
 {
+    private readonly Dictionary<StockKeepingUnit, IPricingStrategy> _strategies;
+
+    public PricingStrategyIndex()
+    {
+        _strategies = [];
+    }
     public void SetStrategy(StockKeepingUnit sku, IPricingStrategy pricingStrategy)
     {
-        throw new NotImplementedException(nameof(SetStrategy));
+        if (sku == StockKeepingUnit.INVALID)
+            throw new InvalidEnumArgumentException(nameof(sku));
+
+        _strategies[sku] = pricingStrategy;
     }
 
     public IPricingStrategy GetStrategyForStockKeepingUnit(StockKeepingUnit sku)
     {
-        throw new NotImplementedException(nameof(GetStrategyForStockKeepingUnit));
+        if (sku == StockKeepingUnit.INVALID)
+            throw new InvalidEnumArgumentException(nameof(sku));
+
+        if (!_strategies.TryGetValue(sku, out var strategy))
+            throw new KeyNotFoundException($"No pricing strategy found for SKU: {sku}");
+
+        return strategy;
     }
 }
